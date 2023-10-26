104300ebd7cc095cf21170b14b95134ba0393135
Somin Shim <60233499+sominshim@users.noreply.github.com>
2023-10-26 15:01:34 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.py
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.py
@@ -1,34 +1,38 @@
 def solution(line):
     point = set()
+    x_min = y_min = int(1e15)
+    x_max = y_max = -int(1e15)
+
     # 1. 해를 구한다.
-    Xmin, Xmax= int(1e15), int(-1e15)
-    Ymin, Ymax= int(1e15), int(-1e15)
     for i in range(len(line)):
+        a, b, e = line[i]
         for j in range(i+1, len(line)):
-            # 해가 없을 경우 예외처리(평행) > AD - BC = 0
-            ad_bc = line[i][0]*line[j][1] - line[i][1]*line[j][0]
-            if ad_bc == 0:
-                continue
-            x = (line[i][1]*line[j][2] - line[i][2]*line[j][1]) / ad_bc
-            y = (line[i][2]*line[j][0] - line[i][0]*line[j][2]) / ad_bc
+            c, d, f = line[j]
+            # 해가 없을 경우 예외처리(평행)
+            if a * d == b * c: continue
+            x = (b * f - e * d) / (a * d - b * c)
+            y = (e * c - a * f) / (a * d - b * c)
 
             # 정수 확인 & 최대, 최소 x, y 좌표
-            if (x==int(x)) and (y==int(y)):
+            if x == int(x) and y == int(y):
+                x = int(x)
+                y = int(y)
                 point.add((x, y))
 
-                if Xmin > x: Xmin=x
-                if Xmax < x: Xmax=x
-                if Ymin > y: Ymin=y
-                if Ymax < y: Ymax=y
+                if x_min > x: x_min=x
+                if x_max < x: x_max=x
+                if y_min > y: y_min=y
+                if y_max < y: y_max=y
 
     # 2. 최대, 최소 x, y간의 거리를 구한다 -> 배열을 .으로 초기화한다
-    answer = [['.' for _ in range(int(Xmax-Xmin)+1)] for _ in range(int(Ymax-Ymin)+1)]
-    # pprint(answer)
+    x_len = x_max - x_min + 1
+    y_len = y_max - y_min + 1
+    answer = [['.'] * x_len for _ in range(y_len)]
 
     # 3. 최솟값 x를 전체 교점에서 - 빼고, 최댓값 y를 전체 교점에서 -빼고 절댓값을 씌운다.
     # -> 계산된 교점을 *으로 바꾼다.
     for x, y in point:
-        answer[int(abs(y-Ymax))][int(x-Xmin)] = '*'
+        answer[int(abs(y-y_max))][int(x-x_min)] = '*'
 
     for i in range(len(answer)):
         answer[i]="".join(answer[i])

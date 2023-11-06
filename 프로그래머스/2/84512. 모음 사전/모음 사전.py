927927e41eeef13667b0d64416cc6de3092d8c22
Somin Shim <60233499+sominshim@users.noreply.github.com>
2023-11-06 16:46:39 +0900

    [level 2] Title: 모음 사전, Time: 4.68 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,5 +1,12 @@
 def create_dict(dictionary=[] , char='', step=0):
-    if step == 6: return 
+    '''
+    1~5자리 대문자 모음 사전(dictionary)을 return
+    
+    dictionary: return할 사전 리스트
+    char: 사전에 추가할 단어
+    step: 단어 자릿수
+    '''
+    if step == 6: return
     if char != '':
         dictionary.append(char)
 
@@ -7,6 +14,9 @@ def create_dict(dictionary=[] , char='', step=0):
         create_dict(dictionary, ''.join([char, i]), step + 1)
         
 def solution(word):
+    '''
+    입력된 word가 사전에서 몇 번째 단어인지 return
+    '''
     dictionary = []
     create_dict(dictionary, '', 0)
     return dictionary.index(word) + 1

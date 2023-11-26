1c1cbd8c8d893deb81fdb4551616e65650ef3e31
Somin Shim <60233499+sominshim@users.noreply.github.com>
2023-11-26 17:30:51 +0900

    [level 3] Title: 정수 삼각형, Time: 34.39 ms, Memory: 14.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,11 +1,7 @@
 def solution(triangle):
     for i in range(len(triangle) - 1, 0, -1):
-        # 왼쪽, 오른쪽 중 큰 값 저장
-        max_bottom = []
         for j in range(len(triangle[i]) - 1):
-            max_bottom.append(max(triangle[i][j],triangle[i][j + 1]))
-        # up stage 에 더하기
-        triangle[i - 1] = [x + y for x, y in zip(triangle[i - 1], max_bottom)]
-        
-    # print(triangle)
+            # up stage 에 더하기
+            triangle[i - 1][j] += max(triangle[i][j],triangle[i][j + 1])
+
     return triangle[0][0]

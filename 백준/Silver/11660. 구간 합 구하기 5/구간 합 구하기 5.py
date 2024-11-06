d2b0e303122b92b6008f0fffa3e119a9bee674a6
Somin Shim <60233499+sominshim@users.noreply.github.com>
2024-11-06 13:04:42 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 940 ms, Memory: 105344 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,19 +1,25 @@
+# https://www.acmicpc.net/problem/11660
+# 구간 합 5
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+n, m = map(int, input().split()) # 배열 크기, 질의 개수
+A = [[0] * (n+1)] # 원본 배열
+D = [[0]*(n+1) for _ in range(n+1)] # 합 배열
 
-# 2차원 누적합 배열 dp 초기화
-dp = [[0] * (n + 1) for _ in range(n + 1)]
+# A 원본 배열 입력
+for i in range(n):
+    A_row = [0] + [int(x) for x in input().split()]
+    A.append(A_row)
 
-# dp 배열 채우기
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        dp[i][j] = arr[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+# D 합 배열 입력
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        D[i][j] = D[i-1][j] + D[i][j-1] - D[i-1][j-1] + A[i][j]
 
-# M줄에 걸쳐 구간 합 계산하기
+# M개의 질의에 대한 결과값 출력
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    result = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]
-    print(result)
+    ans = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1]
+    print(ans)
+

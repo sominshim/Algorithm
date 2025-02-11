1275955ae0973560e3eda86a9b5f75cea2ed0fde
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-02-11 11:24:30 +0900

    [level 2] Title: 전화번호 목록, Time: 100.98 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,7 @@
 def solution(phone_book):
     phone_book.sort()
-
-    for i in range(len(phone_book) - 1):
-        num_len = len(phone_book[i])
-        if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
+    for i in range(1, len(phone_book)):
+        prev_number = phone_book[i-1]
+        if prev_number == phone_book[i][:len(prev_number)]:
             return False
     return True

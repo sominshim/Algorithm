0adeeadef11734e93ab24947dcc61769fa827ce0
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-02-11 11:10:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 60.69 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,7 @@
 def solution(participant, completion):
     part_dict = {}
-    for i in participant: # 참가자 명단 dict 생성
-        part_dict[i] = part_dict.get(i, 0) + 1
-    for i in completion: # 참가자 명단에서 완주자 명단 제거
-        part_dict[i] -= 1
+    for name in participant: # 참가자 명단 dict 생성
+        part_dict[name] = part_dict.get(name, 0) + 1
+    for name in completion: # 참가자 명단에서 완주자 명단 제거
+        part_dict[name] -= 1
     return dict(map(reversed, part_dict.items()))[1]

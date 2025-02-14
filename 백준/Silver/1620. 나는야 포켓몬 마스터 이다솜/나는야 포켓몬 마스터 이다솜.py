995f0f3a4f4251728a3b6c1980802b099c85ff82
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-02-14 12:23:36 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 184 ms, Memory: 56984 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,16 +1,26 @@
+# 입력: N(포켓몬 개수), M(문제 개수), 포켓몬 이름, 문제
+# 출력: 문제에 대한 답
+#      - 포켓몬 번호 > 포켓몬 이름 출력
+#      - 포켓몬 이름 > 포켓몬 번호 출력
 import sys
 
-n, m = map(int, input().split())
-pocketmon = {}
+n, m = map(int, sys.stdin.readline().split())
+# 포켓몬 이름 → 번호, 번호 → 이름 딕셔너리 생성
+name_to_num = {}
+num_to_name = {}
+
 for i in range(1, n+1):
     name = sys.stdin.readline().strip()
-    pocketmon[name] = i
+    name_to_num[name] = i
+    num_to_name[i] = name
 
-pocketmon_name = list(pocketmon.keys())
-for i in range(m):
-    prob = sys.stdin.readline().strip()
-    try: # 포켓몬 번호 입력
-        num = int(prob)
-        print(pocketmon_name[num-1])
-    except: # 포켓몬 이름 입력
-        print(pocketmon[prob])
+output = []
+for _ in range(m):
+    query = sys.stdin.readline().strip()
+    if query.isdigit():  # 숫자 입력
+        output.append(num_to_name[int(query)])
+    else:  # 문자열 입력
+        output.append(str(name_to_num[query]))
+
+# 한 번에 출력하여 성능 개선
+sys.stdout.write("\n".join(output) + "\n")

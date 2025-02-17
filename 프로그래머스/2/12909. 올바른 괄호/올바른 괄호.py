29e20355510e277a7859ee1cce1574b65d6659a6
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-02-17 16:55:30 +0900

    [level 2] Title: 올바른 괄호, Time: 10.14 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,12 +1,13 @@
 # 문자열 s가 올바른 괄호이면 true, 아니면 false return
 def solution(s):
     stack = []
-    for value in s:
-        if (len(stack) != 0) and (value == ")") and (stack[-1] == "("):
+    for char in s:
+        if stack and stack[-1] == "(" and char == ")":
             stack.pop()
         else:
-            stack.append(value)
-            
+            stack.append(char)
+
+    # 스택에 남아있는 괄호가 있다면 False return        
     if len(stack) != 0:
         return False
                

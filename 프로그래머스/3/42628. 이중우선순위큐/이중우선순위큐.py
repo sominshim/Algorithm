a26c9ea74c438ba12ab0fdc0a0f38727eea47510
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-02-27 00:15:21 +0900

    [level 3] Title: 이중우선순위큐, Time: 76.78 ms, Memory: 22.2 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,39 +1,43 @@
 import heapq
 def solution(operations):
-    valid = {}
-    length = 0
+    min_heap, max_heap = [], []
+    valid = {} # 값의 존재 유무 → True/False
+    length = 0 # 힙의 길이
+
     for operation in operations:
+        # 현재 저장된 원소가 없으면 힙 초기화
         if length == 0:
-            min_heap = []
-            max_heap = []
-        o, v = operation.split(' ')
-        v = int(v)
-        if o == "I": # 입력
-            heapq.heappush(min_heap, v)
-            heapq.heappush(max_heap, -v)
-            valid[v] = True
-            valid[-v] = True
+            min_heap, max_heap = [], []
+
+        command, value = operation.split(' ')
+        value = int(value)
+
+        if command == "I": # 삽입
+            heapq.heappush(min_heap, value)
+            heapq.heappush(max_heap, -value)
+            valid[value] = True
+            valid[-value] = True
             length += 1
-        elif length > 0:
-            if v == -1: # 최솟값 삭제
+        elif length > 0: # 삭제
+            if value == -1:  # 최솟값 삭제
                 removed = heapq.heappop(min_heap)
                 valid[removed] = False
                 valid[-removed] = False
-            else:# 최댓값 삭제
+            else:  # 최댓값 삭제
                 removed = heapq.heappop(max_heap)
                 valid[removed] = False
                 valid[-removed] = False
             length -= 1
 
-    def search_vaild(heap, valid):
+    # 힙에서 유효한 값을 찾는 함수 
+    def search_valid(heap, valid):
         while heap:
             v = heapq.heappop(heap)
             if valid[v] == True:
                 return v
         return 0
     
-    answer = []
-    answer.append(-search_vaild(max_heap, valid))
-    answer.append(search_vaild(min_heap, valid))
+    max_val = -search_valid(max_heap, valid)
+    min_val = search_valid(min_heap, valid)
     
-    return answer
+    return [max_val, min_val]

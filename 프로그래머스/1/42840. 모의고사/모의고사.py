359ccffefed20bffe9c604484d3e5b7d1dd964e3
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-03-10 13:40:29 +0900

    [level 1] Title: 모의고사, Time: 3.30 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,25 +1,22 @@
 def solution(answers):
-    answer = []
-    person = [0] * 3 # 3명의 수포자가 맞춘 개수를 넣을 리스트
-    a1 = [1, 2, 3, 4, 5] # 1번째 수포자의 답을 넣은 리스트
-    a2 = [2, 1, 2, 3, 2, 4, 2, 5] # 2번째 수포자의 답을 넣은 리스트
-    a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] # 3번째 수포자의 답을 넣은 리스트
+    """가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return"""
+    A = [1, 2, 3, 4, 5]
+    B = [2, 1, 2, 3, 2, 4, 2, 5]
+    C = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
-    # 맞춘 개수를 확인하는 코드
+    score = [0] * 3
     for i in range(len(answers)):
-        # 1번째 수포자는 5개의 답이 반복되므로 [i%5] 해줌
-        if answers[i] == a1[i%5]:
-            person[0] += 1
-        # 2번째 수포자는 8개의 답이 반복되므로 [i%5] 해줌
-        if answers[i] == a2[i%8]:
-            person[1] += 1
-        # 3번째 수포자는 10개의 답이 반복되므로 [i%5] 해줌
-        if answers[i] == a3[i%10]:
-            person[2] += 1
-
-    winner = max(person) # 가장 많이 맞춘 사람의 개수를 넣는 변수 
-    # 가장 많이 맞춘 사람을 넣는 코드 (공동 1등이 나올 수 있으므로)
-    for i in range(len(person)):
-        if person[i] == winner:
-            answer.append(i+1)
-    return answer
+        answer = answers[i]
+        if A[i%5] == answer:
+            score[0] += 1
+        if B[i%8] == answer:
+            score[1] += 1
+        if C[i%10] == answer:
+            score[2] += 1
+    
+    first_rank = []
+    max_score = max(score)
+    for i, score in enumerate(score):
+        if score == max_score:
+            first_rank.append(i+1)
+    return first_rank

03c6d2aed03617e54aecb592f598c8318c57e044
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-04-01 13:34:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.55 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,31 +1,37 @@
 from collections import deque
 
 def solution(maps):
-    q = deque([(0,0)])
     n, m = len(maps), len(maps[0])
-    visited = set()
-    direction = [[-1, 0], # 위
-                 [1, 0], # 아래
-                 [0, 1], # 오른쪽
-                 [0, -1] # 왼쪽
-                 ]
+    q = deque([(0,0)])
+    visited = set([(0, 0)])  # 방문한 좌표 저장
+    
+    # 상, 하, 좌, 우 이동 방향
+    directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+
     while q:
-        cur_r, cur_c = q.popleft()
+        curr_r, curr_c = q.popleft()
 
-        for r, c in direction:
-            new_r =  cur_r + r
-            new_c = cur_c + c
+        for r, c in directions:
+            next_r =  curr_r + r
+            next_c = curr_c + c
 
-            if 0 <= new_r < n and \
-                0 <= new_c < m and \
-                maps[new_r][new_c] == 1 and \
-                (new_r, new_c) not in visited:
-                
-                maps[new_r][new_c] += maps[cur_r][cur_c]
-                visited.add((new_r, new_c))
-                q.append((new_r, new_c))
+            # 범위 내에 있고, 이동 가능한 경로(1)이며, 방문하지 않은 경우
+            if (
+                0 <= next_r < n and
+                0 <= next_c < m and
+                maps[next_r][next_c] == 1 and
+                (next_r, next_c) not in visited
+            ):
+                # 이전 위치에서 +1 한 값으로 거리 갱신
+                maps[next_r][next_c] = maps[curr_r][curr_c] + 1
+                visited.add((next_r, next_c))
+                q.append((next_r, next_c))
+            
+            if next_r == n-1 and next_c == m-1:
+                return maps[n-1][m-1]
 
-    return maps[-1][-1] if (n-1, m-1) in visited else -1
+    # 마지막까지 도달하지 못했을 때
+    return -1
 
 if __name__ == "__main__":
     maps = [[1,0,1,1,1],

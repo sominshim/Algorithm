34611ac02d99614c42db9c5ff3e93816c61f83de
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-04-04 10:20:15 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,34 @@
+
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+from collections import defaultdict
+def solution(tickets):
+    r = defaultdict(list)
+    for i,j in tickets:
+        r[i].append(j)
+    for i in r.keys():
+        r[i].sort()
+
+    s = ["ICN"]
+    p = []
+    while s:
+        q = s[-1]
+        if r[q] != []:
+            s.append(r[q].pop(0))
+        else:
+            p.append(s.pop())
+    return p[::-1]

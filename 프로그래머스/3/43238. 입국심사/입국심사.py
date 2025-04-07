2144ed7fd94beba65fcc00641a26a2117ed63cdb
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-04-07 11:55:59 +0900

    [level 3] Title: 입국심사, Time: 499.32 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,20 +1,32 @@
 def solution(n, times):
+    """
+    input:
+        - n: 입국심사를 기다리는 사람 수
+        - times: 각 심사관이 한 명을 심사하는데 결리는 시간 배열
+    output:
+        모든 사람이 심사를 받는데 걸리는 시간의 최솟값
+    """
     answer = 0
-    left, right = 1, max(times) * n
-
+    left, right = 1, min(times) * n
     while left <= right:
         mid = (left + right) // 2
-
-        # mid 시간 내에 몇 명을 처리할 수 있는지 계산
+        
+        # mid 시간까지 심사 가능한 사람 수 계산
         people = 0
         for time in times:
             people += mid // time
-            if people >= n: break
+            if people >= n: 
+                answer = mid
+                break
 
         if people >= n:
-            answer = mid
             right = mid - 1
         else:
             left = mid + 1
 
     return answer
+
+if __name__ == "__main__":
+    n = 6
+    times = [7, 10]
+    print(solution(n, times))

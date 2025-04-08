d34c0a9c8d760dad62b2ef17ad8ae005cd751cef
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-04-08 15:54:52 +0900

    [Silver IV] Title: 수 찾기, Time: 428 ms, Memory: 49160 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,21 +1,53 @@
-def binary_search(array, target, start, end):
-    if start > end:
-        return print(0)
-    
-    mid = (start + end) // 2
-    if target == array[mid]:
-        return print(1)
-    elif target < array[mid]:
-        return binary_search(array, target, start, mid-1)
-    elif target > array[mid]:
-        return binary_search(array, target, mid+1, end)
+import sys
 
-n = int(input())
-array = list(map(int, input().split()))
+def binary_search(n, a_arr, target):
+    left, right = 0, n-1
 
-m = int(input())
-input_arr = list(map(int, input().split()))
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if a_arr[mid] == target:
+            return print(1)
+        
+        elif a_arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
 
-array.sort()
-for i in range(m):
-    binary_search(array, input_arr[i], 0, n-1)
+    return print(0)
+
+def solution(n, m, a_arr, b_arr):
+    """
+    Parameters:
+        - n (int): a_arr 배열의 길이
+        - m (int): b_arr 배열의 길이
+        - a_arr (List[int]): n개의 정수 배열
+        - b_arr (List[int]): m개의 정수 배열
+    Returns:
+        - b_arr 배열 안에 있는 정수가 a_arr 배열에 존재유무 출력. 존재하면 1, 아니면 0 (int)
+    """
+    a_arr.sort()
+
+    for target in b_arr:
+        left, right = 0, n-1
+
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if a_arr[mid] == target:
+                print(1)
+                break
+            
+            elif a_arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        else: print(0)
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+    a_arr = list(map(int, sys.stdin.readline().split()))
+    m = int(sys.stdin.readline())
+    b_arr = list(map(int, sys.stdin.readline().split()))
+    solution(n, m, a_arr, b_arr)

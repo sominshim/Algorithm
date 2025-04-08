9c60e3bcb61e8c0c7189660a2decc04c51e9a388
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-04-08 10:53:10 +0900

    [level 4] Title: 징검다리, Time: 142.77 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,21 +1,38 @@
 def solution(distance, rocks, n):
+    """
+    Parameters:
+        - distance (int): 출발지점부터 도착지점까지의 거리
+        - rocks (List[int]): 바위들이 있는 위치를 담은 배열
+        - n (int): 제거할 바위의 수
+    Returns:
+        - 바위를 n개 제거한 뒤 각 지점 사이의 거리의 최솟값 중에 가장 큰 값 (int)
+    """
+    start, end = 0, distance
+    rocks.sort()
+    rocks.append(distance)
     answer = 0
-    sorted_rocks = sorted(rocks)
-    sorted_rocks.append(distance)
-    left = 0
-    right = distance
-    while (left <= right):
-        mid = int((left + right) / 2)
-        cnt = 0
-        p = 0
-        for i in range(len(sorted_rocks)):
-            if (sorted_rocks[i] - p < mid):
-                cnt += 1
+   
+    while start <= end:
+        mid = (start + end) // 2 # mid = 두 지점의 최소거리 
+        
+        delete_cnt = 0
+        pre_rock = 0
+        for rock in rocks:
+            if rock - pre_rock < mid: 
+                delete_cnt += 1
             else:
-                p = sorted_rocks[i]
-        if cnt > n:
-            right = mid - 1
-        else:
-            left = mid + 1
+                pre_rock = rock
+
+        if delete_cnt <= n:
             answer = mid
+            start = mid + 1
+        else:
+            end = mid - 1
+
     return answer
+
+if __name__ == "__main__":
+    distance = 25
+    rocks = [2, 14, 11, 21, 17]	
+    n = 2
+    print(solution(distance, rocks, n)) # 4

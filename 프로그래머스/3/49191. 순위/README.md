03f02705ed3e61d7602026318063ebef70e870ba
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-04-16 12:50:15 +0900

    [level 3] Title: 순위, Time: 6.38 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/README.md
+++ 프로그래머스/3/49191. 순위/README.md
@@ -1,10 +1,10 @@
 # [level 3] 순위 - 49191 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191) 
 
 ### 성능 요약
 
-메모리: 11.1 MB, 시간: 9.81 ms
+메모리: 10.1 MB, 시간: 6.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 1일 17:1:59
+2025년 04월 16일 12:50:13
 
 ### 문제 설명
 
--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,21 +1,33 @@
 from collections import defaultdict
-
 def solution(n, results):
-    answer = 0
-    win, lose = defaultdict(set), defaultdict(set)
-
+    """
+    Parameters:
+        - n (int): 선수의 수
+        - results (List[int]): 경기 결과를 담은 2차원 배열
+    Returns:
+        - 정확하게 순위를 매길 수 있는 선수의 수 (int)
+    """
+    win = defaultdict(set)
+    lose = defaultdict(set)
     for winner, loser in results:
         win[loser].add(winner)
         lose[winner].add(loser)
 
-    for i in range(1, n + 1):
+    # lose + win 개수 == n-1 이면 순위 확정
+    for i in range(1, n+1):
         for winner in win[i]:
             lose[winner].update(lose[i])
         for loser in lose[i]:
             win[loser].update(win[i])
-    
-    for i in range(1, n + 1):
-        if (len(win[i]) + len(lose[i])) == (n - 1) : 
+    answer = 0
+    for i in range(1, n+1):
+        if len(win[i]) + len(lose[i]) == n-1:
             answer += 1
-        
+
     return answer
+
+if __name__ == "__main__":
+    n = 5
+    results = [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]
+
+    print(solution(n, results))

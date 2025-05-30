0762edf891e5ec8201a944a068cefb255b48b39e
Somin Shim <60233499+sominshim@users.noreply.github.com>
2025-05-30 12:09:50 +0900

    [Gold V] Title: 퇴사 2, Time: 1748 ms, Memory: 169000 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,20 +1,21 @@
 import sys
 
-def max_profit(N, schedules):
-    dp = [0] * (N + 2)  # N+1일까지 고려
-
+def solution(N, time, pay):
+    dp = [0] * (N + 1)
     for i in range(1, N + 1):
-        Ti, Pi = schedules[i - 1]
-        # 상담을 선택하지 않는 경우
         dp[i] = max(dp[i], dp[i - 1])
-        # 상담을 선택하는 경우
-        if i + Ti <= N + 1:
-            dp[i + Ti] = max(dp[i + Ti], dp[i] + Pi)
-
-    return max(dp)
+        end_date = i + time[i - 1] - 1
+        if end_date <= N:
+            dp[end_date] = max(dp[end_date], dp[i - 1] + pay[i - 1])
+    
+    return dp[N]
 
 if __name__ == "__main__":
-    input = sys.stdin.readline
-    N = int(input())
-    schedules = [tuple(map(int, input().split())) for _ in range(N)]
-    print(max_profit(N, schedules))
+    N = int(sys.stdin.readline())
+    time, pay = [], []
+    for _ in range(N):
+        T, P = map(int, sys.stdin.readline().split())
+        time.append(T)
+        pay.append(P)
+    
+    print(solution(N, time, pay))    
